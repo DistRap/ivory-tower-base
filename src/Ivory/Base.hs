651a67b5d428dc8b9222de63646b68d84d714c8e
Richard Marko <srk@48.io>
2018-04-23 22:50:47 +0200

    add ixToU8 and ixToU16 shortcuts

--- src/Ivory/Base.hs
+++ src/Ivory/Base.hs
@@ -1,1 +1,9 @@
 module Ivory.Base where
+
+import Ivory.Language
+
+ixToU8 :: ANat n => Ix n -> Uint8
+ixToU8 = (bitCast :: Uint32 -> Uint8) . signCast . fromIx
+
+ixToU16 :: ANat n => Ix n -> Uint16
+ixToU16 = (bitCast :: Uint32 -> Uint16) . signCast . fromIx
